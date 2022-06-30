1ed81fc7eb1bebb764c9499878a2e692350df714
Fredy Rico <frico3@estudiantes.areandina.edu.co>
2022-06-29 20:40:12 -0500

    Mi tercera funcion, Agosto

--- funciones.py
+++ funciones.py
@@ -1,8 +1,21 @@
 import os
 os.system("cls")
 
-#MI PRIMERA FUNCION 
 
 
-def nombre():
-    pass
+
+def nombre(): #MI PRIMERA FUNCION 
+    a=3
+    b=5
+    c=a+b
+    print("El resultado de la suma es: ",c)
+nombre()
+
+    
+def resta():
+    a=2
+    b=1
+    c=a-b
+    print("El resultado de la resta es: ",c)
+resta()
+    
